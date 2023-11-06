cef3df4aeff4898d468beee98b52ab583b405b2d
delchev <delchevn@gmail.com>
2023-11-06 14:56:04 +0200

    handlers locations fixed

--- codbex-perseus/gen/dao/SalesInvoices/SalesInvoiceItem.js
+++ codbex-perseus/gen/dao/SalesInvoices/SalesInvoiceItem.js
@@ -12,47 +12,48 @@ let dao = daoApi.create({
 			id: true,
 			autoIncrement: true,
 		},
- {
+		{
 			name: "SalesInvoice",
 			column: "SALESINVOICEITEM_SALESINVOICE",
 			type: "INTEGER",
 		},
- {
+		{
 			name: "Name",
 			column: "SALESINVOICEITEM_NAME",
 			type: "VARCHAR",
 		},
- {
+		{
 			name: "Quantity",
 			column: "SALESINVOICEITEM_QUANTITY",
 			type: "DOUBLE",
 		},
- {
+		{
 			name: "Price",
 			column: "SALESINVOICEITEM_PRICE",
 			type: "DOUBLE",
 		},
- {
+		{
 			name: "Amount",
 			column: "SALESINVOICEITEM_AMOUNT",
 			type: "DOUBLE",
 		}
-]
+	]
 });
 
-exports.list = function(settings) {
+exports.list = function (settings) {
 	return dao.list(settings);
 };
 
-exports.get = function(id) {
+exports.get = function (id) {
 	return dao.find(id);
 };
 
-exports.create = function(entity) {
+exports.create = function (entity) {
 	entity["Amount"] = entity["Quantity"] * entity["Price"];
 	let id = dao.insert(entity);
 	triggerEvent("Create", {
 		table: "CODBEX_SALESINVOICEITEM",
+		entity: entity,
 		key: {
 			name: "Id",
 			column: "SALESINVOICEITEM_ID",
@@ -62,11 +63,12 @@ exports.create = function(entity) {
 	return id;
 };
 
-exports.update = function(entity) {
+exports.update = function (entity) {
 	entity["Amount"] = entity["Quantity"] * entity["Price"];
 	dao.update(entity);
 	triggerEvent("Update", {
 		table: "CODBEX_SALESINVOICEITEM",
+		entity: entity,
 		key: {
 			name: "Id",
 			column: "SALESINVOICEITEM_ID",
@@ -75,10 +77,12 @@ exports.update = function(entity) {
 	});
 };
 
-exports.delete = function(id) {
+exports.delete = function (id) {
+	entity = dao.find(id);
 	dao.remove(id);
 	triggerEvent("Delete", {
 		table: "CODBEX_SALESINVOICEITEM",
+		entity: entity,
 		key: {
 			name: "Id",
 			column: "SALESINVOICEITEM_ID",
@@ -99,7 +103,7 @@ exports.count = function (SalesInvoice) {
 	return 0;
 };
 
-exports.customDataCount = function() {
+exports.customDataCount = function () {
 	let resultSet = query.execute('SELECT COUNT(*) AS COUNT FROM "CODBEX_SALESINVOICEITEM"');
 	if (resultSet !== null && resultSet[0] !== null) {
 		if (resultSet[0].COUNT !== undefined && resultSet[0].COUNT !== null) {
